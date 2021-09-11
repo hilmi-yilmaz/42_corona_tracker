0ef3fb87b056997a0d670fa242c0e1958a3a9ed1
hilmi-yilmaz <hilmiyilmaz.1998@gmail.com>
2021-09-11 13:00:00 +0200

    added interval time and created a list to store recently logged off people, made some small changes to DatabaseOperations class

--- deamon.py
+++ deamon.py
@@ -3,6 +3,10 @@ import time
 from get_user_data import API42
 from handle_database import DatabaseOperations
 
+# Set interval variable for while loop
+# If someone logs in and off in less time than the interval perdiod, that person will not be registered
+interval = 600
+
 # Instantiate a DatabaseOperations object to handle MySQL
 db_operations = DatabaseOperations("codam_corona_tracker", "hilmi", "hilmi")
 
@@ -12,23 +16,24 @@ payload = {"filter[campus_id]":14, "filter[active]": "true", "page[size]": 50}
 
 # Continously get data from the API and put into database
 while True:
-    data = api.get("locations", payload) # data is a list
+	data = api.get("locations", payload) # data is a list
 
-    # Get the active people data
-    currently_active = db_operations.get_active_students(data)
-    print(currently_active)
+	# Get active people data
+	currently_active = db_operations.get_active_students(data)
+	print(f"Currently active students: {currently_active}")
 
-    # Get the recently logged off session
-    logged_off = db_operations.get_recently_logged_off(currently_active)
+	# Get the recently logged off session
+	logged_off = db_operations.get_recently_logged_off(currently_active)
 
-    # Check whether someone logged off, if so, query and add to the database
-    for session in logged_off:
-        tmp_payload = payload
-        tmp_payload["filter[id]"] = session
-        who_logged_off = api.get("locations", tmp_payload)
-        print(who_logged_off)
+	# Check whether someone logged off, if so, query and add to the database
+	who_logged_off = []
+	for session in logged_off:
+		tmp_payload = payload
+		tmp_payload["filter[id]"] = session
+		who_logged_off.append(api.get("locations", tmp_payload))
+	print(f"People who logged off in the last {interval} minutes: {who_logged_off}")
 
-    db_operations.insert_data(data, currently_active)
+	db_operations.insert_data(who_logged_off)
 
-    db_operations.active = currently_active
-    time.sleep(60)
+	db_operations.active = currently_active
+	time.sleep(interval)
--- handle_database.py
+++ handle_database.py
@@ -6,114 +6,100 @@ from mysql.connector import connect, Error
 
 
 class DatabaseOperations:
-    """
-    Handles all operations related to the mysql database.
-    """
+	"""
+	Handles all operations related to the mysql database.
+	"""
 
-    def __init__(self, db_name, user, password):
-        self.db_name = db_name
-        self.user = user
-        self.password = password
-        self.connector = self.connect_to_database()
-        self.cursor = self.connector.cursor()
-        self.active = [] # Contains login session id
+	def __init__(self, db_name, user, password):
+		self.db_name = db_name
+		self.user = user
+		self.password = password
+		self.connector = self.connect_to_database()
+		self.cursor = self.connector.cursor()
+		self.active = [] # Contains login session id
 
-    def connect_to_database(self):
-        try:
-            connection = connect(
-                host="localhost",
-                user=self.user,
-                password=self.password,
-                database=self.db_name)
-            if connection.is_connected():
-                db_info = connection.get_server_info()
-                print(f"Connected to MySQL server version {db_info}")
-        except Error as e:
-            print(f"Cannot connect to {self.db_name}: {e}")
-            sys.exit(1)
-        print(f"Succesfully connected to database {self.db_name}.")
-        return (connection)
+	def connect_to_database(self):
+		try:
+			connection = connect(
+				host="localhost",
+				user=self.user,
+				password=self.password,
+				database=self.db_name)
+			if connection.is_connected():
+				db_info = connection.get_server_info()
+				print(f"Connected to MySQL server version {db_info}")
+		except Error as e:
+			print(f"Cannot connect to {self.db_name}: {e}")
+			sys.exit(1)
+		print(f"Succesfully connected to database {self.db_name}.")
+		return (connection)
 
-    def get_active_students(self, data):
-        """
-        Get the currently active people.
-        """
-        currently_active = [] # the people that are currently active, this can be compared to self.active
-        for user in data:
-            currently_active.append(user["id"])
-        return (currently_active)
+	def get_active_students(self, data):
+		"""
+		Get the currently active people.
+		"""
+		currently_active = [] # the people that are currently active, this can be compared to self.active
+		for user in data:
+			currently_active.append(user["id"])
+		return (currently_active)
 
-    def read_data(self, query):
-        self.cursor.execute(query)
-        result = self.cursor.fetchall()
-        for row in result:
-            print(row)
+	def get_recently_logged_off(self, currently_active):
+		"""
+		Get the session id that just logged off. This session will be put into the database.
+		"""
+		logged_off = list(set(self.active) - set(currently_active))
+		return (logged_off)
 
-    def get_recently_logged_off(self, currently_active):
-        """
-        Get the session id that just logged off. This session will be put into the database.
-        """
-        logged_off = list(set(self.active) - set(currently_active))
-        return (logged_off)
+	def read_data(self, query):
+		self.cursor.execute(query)
+		result = self.cursor.fetchall()
+		for row in result:
+			print(row)
 
+	def insert_data(self, who_logged_off):
+		# Create table if table doesn't exist yet
+		self.extract_hosts(who_logged_off)
 
+		# Insert user data
+		for user in who_logged_off:
+			host = user["host"][:-9]
+			#begin_at = datetime.strptime(user["begin_at"],  "%Y-%m-%dT%H:%M:%S.%fZ")
+			#end_at = datetime.strptime(user["end_at"],  "%Y-%m-%dT%H:%M:%S.%fZ")
+			insert_login_session_query = """
+			INSERT INTO {}
+			(session_id, login, begin_at, end_at)
+			VALUES (%s, %s, %s, %s)
+			""".format(host)
+			#print(insert_login_session_query)
+			self.cursor.execute(insert_login_session_query, [user["id"], user["user"]["login"], user["begin_at"], user["end_at"]])
+		self.connector.commit()
+			
 
-    def insert_data(self, data, currently_active):
-        # Create table if table doesn't exist yet
-        self.extract_hosts(data)
+	def extract_hosts(self, data):
+		"""
+		Extract the hosts from the data (for example f0r1s6.codam.nl).
+		If there is no table for this specific host yet, create one.
+		"""
+		for user in data:
+			host = user["host"][:-9]
+			query = "show tables like \'" + str(host) + "\';"
+			self.cursor.execute(query)
+			result = self.cursor.fetchone()
+			if not result:
+				#print("{} doesn't exist as a table. Creating...".format(host))
+				self.create_host_table(host)
 
-        # Get the difference between the active and currently_active people
-        logged_off = list(set(self.active) - set(currently_active))
-        #print(logged_off)
-
-        # Request the api for the session which logged off
-        
-
-        # Insert user data
-        # for user in data:
-
-        #     host = user["host"][:-9]
-        #     if (user["id"] not in self.active):
-        #         #begin_at = datetime.strptime(user["begin_at"],  "%Y-%m-%dT%H:%M:%S.%fZ")
-        #         #end_at = datetime.strptime(user["end_at"],  "%Y-%m-%dT%H:%M:%S.%fZ")
-        #         insert_login_session_query = """
-        #         INSERT INTO {}
-        #         (session_id, login, begin_at, end_at)
-        #         VALUES (%s, %s, %s, %s)
-        #         """.format(host)
-        #         #print(insert_login_session_query)
-        #         self.cursor.execute(insert_login_session_query, [user["id"], user["user"]["login"], user["begin_at"], user["end_at"]])
-        # self.connector.commit()
-            
-
-    def extract_hosts(self, data):
-        """
-        Extract the hosts from the data (for example f0r1s6.codam.nl).
-        If there is no table for this specific host yet, create one.
-        """
-        for user in data:
-            host = user["host"][:-9]
-            query = "show tables like \'" + str(host) + "\';"
-            self.cursor.execute(query)
-            result = self.cursor.fetchone()
-            if result:
-                #print("{} does exist as a table".format(host))
-                pass
-            else:
-                #print("{} doesn't exist as a table. Creating...".format(host))
-                self.create_host_table(host)
-
-    def create_host_table(self, host_name):
-        """
-        Create a table with the host_name.
-        """
-        create_host_table_query = '''
-        CREATE TABLE {}(
-            session_id INT,
-            login VARCHAR(100),
-            begin_at VARCHAR(100),
-            end_at VARCHAR(100)
-        )
-        '''.format(host_name)
-        self.cursor.execute(create_host_table_query)
-        self.connector.commit()
+	def create_host_table(self, host_name):
+		"""
+		Create a table with the host_name.
+		"""
+		create_host_table_query = '''
+		CREATE TABLE {}(
+			session_id INT,
+			login VARCHAR(100),
+			begin_at VARCHAR(100),
+			end_at VARCHAR(100)
+		)
+		'''.format(host_name)
+		self.cursor.execute(create_host_table_query)
+		self.connector.commit()
