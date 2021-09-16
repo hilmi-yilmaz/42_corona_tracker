cb99ea4452b6e3388185ef86cc71386ae527bad7
hilmi <hilmiyilmaz.1998@gmail.com>
2021-09-16 11:49:27 +0200

    restructuring code

--- handle_database.py
+++ handle_database.py
@@ -1,11 +1,11 @@
 import sys
 import time
-from typing import List, Dict
+from typing import List, Dict, Tuple
 from datetime import datetime, timedelta
 from mysql.connector import connect, Error
 
 
-class DatabaseOperations:
+class OperationsDatabase:
 	"""
 	Handles all operations related to the mysql database.
 	Including connecting to the database,
@@ -17,13 +17,13 @@ class DatabaseOperations:
 		password: (str) mysql password
 	"""
 
-	def __init__(self, db_name, user, password):
+	def __init__(self, db_name, table_name, user, password):
 		self.db_name = db_name
+		self.table_name = table_name
 		self.user = user
 		self.password = password
 		self.connector = self.connect_to_database()
 		self.cursor = self.connector.cursor()
-		self.active = []
 
 	def connect_to_database(self):
 		"""
@@ -49,6 +49,137 @@ class DatabaseOperations:
 		print(f"Succesfully connected to database {self.db_name}.")
 		return (connection)
 
+	def read(self, query: str) -> List[Tuple]:
+		"""
+		Read data from database.
+
+		Arguments:
+			qeury: (str) the query to execute against the database.
+
+		Returns:
+			data: (List[Tuple]) data returned by the database.
+		"""
+
+		self.cursor.execute(query)
+		data = self.cursor.fetchall()
+		return (data)
+
+	def insert(self, insert_query: str, insert_data: List[Tuple]) -> None:
+		"""
+		Insert data into database.
+
+		Arguments:
+			insert_query: (str) the query to execute against the database.
+			insert_data: (List[Tuple]) the data to pass within the query.
+
+		Returns:
+			None.
+		"""
+
+		self.cursor.executemany(insert_query, insert_data)
+		self.connector.commit()
+		
+
+	# def get_recently_logged_off(self, logged_in: List[Dict]) -> List[Dict]:
+	# 	"""
+	# 	Get the users that just logged off.
+
+	# 	Arguments:
+	# 		logged_in: (List[Dict]) contains currently logged in users.
+
+	# 	Returns:
+	# 		logged_off: (List[Dict]) contains logged off users.
+	# 	"""
+	# 	logged_off: List[Dict] = []
+	# 	# print(self.active)
+	# 	for user in self.active:
+	# 		if user not in logged_in:
+	# 			logged_off.append(user)
+	# 	return (logged_off)
+
+	# def read_data(self, query: str):
+	# 	"""
+	# 	Query data from the database.
+
+	# 	Arguments:
+	# 		query: (str) contains parameters for the query.
+
+	# 	Returns:
+	# 		None.
+	# 	"""
+	# 	self.cursor.execute(query)
+	# 	return (self.cursor.fetchall())
+
+	# def parse_host_name(self, host: str) -> str:
+	# 	"""
+	# 	Parse the hostname from the host field of the API response.
+	# 	For example: f0r1s1.codam.nl is what 42 API gives back.
+	# 	Only take first part (f0r1s1) for hostname.
+
+	# 	Arguments:
+	# 		host: (str) the host field from the API.
+
+	# 	Returns:
+	# 		host_name: (str) parsed hostname.
+	# 	"""
+	# 	return (host[:-9])
+
+	# def insert_data(self, who_logged_off: List[Dict]) -> None:
+	# 	""" 
+	# 	Inserts the people who logged off into the database.
+
+	# 	Arguments:
+	# 		who_logged_off: (List[Dict]) contains users who logged off.
+
+	# 	Returns:
+	# 		None.
+	# 	"""
+	# 	for user in who_logged_off:
+	# 		host: str = self.parse_host_name(user["host"])
+	# 		begin_at = datetime.strptime(user["begin_at"], "%Y-%m-%dT%H:%M:%S.%fZ") + timedelta(hours=2)
+	# 		end_at = datetime.strptime(user["end_at"], "%Y-%m-%dT%H:%M:%S.%fZ") + timedelta(hours=2)
+	# 		insert_login_session_query: str = """
+	# 		INSERT INTO {}
+	# 		(session_id, login, begin_at, end_at)
+	# 		VALUES (%s, %s, %s, %s)
+	# 		""".format(host)
+	# 		print(f"insert_login_session_query = {insert_login_session_query}")
+	# 		self.cursor.execute(insert_login_session_query, [
+	# 			user["id"], user["user"]["login"], begin_at, end_at])
+	# 	self.connector.commit()
+
+	# def remove_old_data(self, x: int, time_frame: str):
+	# 	"""
+	# 	Removes data from the database that is x timeframes old.
+
+	# 	Arguments:
+	# 		x: (int) amount of time in timeframe.
+	# 		time_frame: (str) can any of week, day, hour.
+
+	# 	Returns:
+	# 		None.
+	# 	"""
+
+	# 	# Get all the tables first
+	# 	response = self.read_data("show tables")
+	# 	all_tables = [table for (table, ) in response]
+
+	# 	for table in all_tables:
+	# 		query_delete_old_data = """
+	# 		delete from {} where end_at < date_sub(now(), interval {} {})
+	# 		""".format(table, x, "hour")
+	# 		self.cursor.execute(query_delete_old_data)
+	# 		self.connector.commit()
+
+class UpdateDatabase(OperationsDatabase):
+	"""
+	Updates the database. Inserts and deletes data.
+	"""
+
+	def __init__(self, db_name, user, password):
+		super().__init__(db_name, user, password)
+		self.active = []
+
 	def get_recently_logged_off(self, logged_in: List[Dict]) -> List[Dict]:
 		"""
 		Get the users that just logged off.
@@ -60,62 +191,10 @@ class DatabaseOperations:
 			logged_off: (List[Dict]) contains logged off users.
 		"""
 		logged_off: List[Dict] = []
-		# print(self.active)
 		for user in self.active:
 			if user not in logged_in:
 				logged_off.append(user)
 		return (logged_off)
-
-	def read_data(self, query: str):
-		"""
-		Query data from the database.
-
-		Arguments:
-			query: (str) contains parameters for the query.
-
-		Returns:
-			None.
-		"""
-		self.cursor.execute(query)
-		return (self.cursor.fetchall())
-
-	def parse_host_name(self, host: str) -> str:
-		"""
-		Parse the hostname from the host field of the API response.
-		For example: f0r1s1.codam.nl is what 42 API gives back.
-		Only take first part (f0r1s1) for hostname.
-
-		Arguments:
-			host: (str) the host field from the API.
-
-		Returns:
-			host_name: (str) parsed hostname.
-		"""
-		return (host[:-9])
-
-	def insert_data(self, who_logged_off: List[Dict]) -> None:
-		""" 
-		Inserts the people who logged off into the database.
-
-		Arguments:
-			who_logged_off: (List[Dict]) contains users who logged off.
-
-		Returns:
-			None.
-		"""
-		for user in who_logged_off:
-			host: str = self.parse_host_name(user["host"])
-			begin_at = datetime.strptime(user["begin_at"], "%Y-%m-%dT%H:%M:%S.%fZ") + timedelta(hours=2)
-			end_at = datetime.strptime(user["end_at"], "%Y-%m-%dT%H:%M:%S.%fZ") + timedelta(hours=2)
-			insert_login_session_query: str = """
-			INSERT INTO {}
-			(session_id, login, begin_at, end_at)
-			VALUES (%s, %s, %s, %s)
-			""".format(host)
-			print(f"insert_login_session_query = {insert_login_session_query}")
-			self.cursor.execute(insert_login_session_query, [
-				user["id"], user["user"]["login"], begin_at, end_at])
-		self.connector.commit()
 
 	def remove_old_data(self, x: int, time_frame: str):
 		"""
@@ -129,13 +208,25 @@ class DatabaseOperations:
 			None.
 		"""
 
-		# Get all the tables first
-		response = self.read_data("show tables")
-		all_tables = [table for (table, ) in response]
+		query_delete_old_data = """
+		delete from {} where end_at < date_sub(now(), interval {} {})
+		""".format(self.table_name, x, "hour")
+		self.cursor.execute(query_delete_old_data)
+		self.connector.commit()
 
-		for table in all_tables:
-			query_delete_old_data = """
-			delete from {} where end_at < date_sub(now(), interval {} {})
-			""".format(table, x, "hour")
-			self.cursor.execute(query_delete_old_data)
-			self.connector.commit()
+	
+
+if __name__ == "__main__":
+
+	db = UpdateDatabase("codam_corona_tracker", "hilmi", "hilmi")
+
+	#db = OperationsDatabase("codam_corona_tracker", "hilmi", "hilmi")
+	query = "select * from f0r1s12"
+	data = db.read(query)
+	print("data\n{}".format(data))
+	print(type(data))
+	print(type(data[0]))
+
+	insert_query = "insert into f0r1s12 (session_id, login, begin_at, end_at) values (%s, %s, %s, %s)"
+	insert_data = [("23331", "yp33333rppr", "1", "2")]
+	db.insert(insert_query, insert_data)
