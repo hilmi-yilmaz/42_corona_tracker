80e91c3d473cb46adf720b046c792b2a8e4f3973
hilmi <hilmiyilmaz.1998@gmail.com>
2021-09-17 11:18:25 +0200

    working query for contact persons

--- get_contact_persons.py
+++ get_contact_persons.py
@@ -1,6 +1,10 @@
 import argparse
+import readline
 
 from handle_database import OperationsDatabase
+
+# Create an instance of DatabaseOperations to query some data from the database
+db_operations = OperationsDatabase("codam_corona_tracker", "data", "hilmi", "hilmi")
 
 # Parse the command line arguments
 parser = argparse.ArgumentParser(
@@ -9,36 +13,27 @@ parser.add_argument("infected_person_login", type=str,
                     help="The intra login of the infected person.")
 parser.add_argument("day_positive", type=str,
                     help="The day the person tested positive. Format: day-month-year e.g. 09-12-2021.")
-parser.add_argument("hosts", type=str, nargs='*',
-					help="The hosts we want to gather data from.")
 args = parser.parse_args()
 print(f"{args.infected_person_login} tested positive on {args.day_positive}.")
-print(f"Gathering data from the following hosts: {args.hosts}")
-
-# Create an instance of DatabaseOperations to query some data from the database
-db_operations = OperationsDatabase("codam_corona_tracker", "f0r1s8", "hilmi", "hilmi")
 
 # Create the query
-query_login = "select * from f0r1s8 where login = \'{}\'".format(
+query_login = "select * from {} where login = \'{}\'".format(db_operations.table_name,
     args.infected_person_login)
 print(query_login)
 
 # Store all the data of the infected person
 data = db_operations.read(query_login)
-print(data)
 
 # Find for each login session of the infected person, which hosts where nearby
 for session in data: # loops over the sessions of the infected person
-	print(session)
-	begin_at = session[2]
-	end_at = session[3]
-	print(type(begin_at))
-	for contact in args.hosts: # loops over all hosts close to this specific session of the infected person
+	begin_at = session[3]
+	end_at = session[4]
+	contacts = input("Which computers do you want to check?\nEnter the hostnames separated by spaces: ").split(" ")
+	if (session[1] in contacts):
+		contacts.remove(session[1])
+	for contact in contacts: # loops over all hosts close to this specific session of the infected person
 		contact_query = """
-		select * from f0r1s8 where (host = \'{}\') and (begin_at or end_at between %s and %s)
-		""".format(contact)
-		contact_data = [begin_at, end_at]
-
-	print(session)
-
-# Make query to all those hosts within timeframe of log in period of infected person
+		select * from {0} where (host = \'{1}\') and ((begin_at between \'{2}\' and \'{3}\') or (end_at between \'{2}\' and \'{3}\'))
+		""".format(db_operations.table_name, contact, begin_at, end_at)
+		data_on_contact_person = db_operations.read(contact_query)
+		print(data_on_contact_person)
