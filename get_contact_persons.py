d0ab48f250a685d23458e30a5a40a7e97409c0cf
hilmi-yilmaz <hilmiyilmaz.1998@gmail.com>
2021-09-17 10:25:27 +0200

    adding contact identification functionality

--- get_contact_persons.py
+++ get_contact_persons.py
@@ -1,6 +1,6 @@
 import argparse
 
-from handle_database import OperationsDatabase, UpdateDatabase
+from handle_database import OperationsDatabase
 
 # Parse the command line arguments
 parser = argparse.ArgumentParser(
@@ -16,18 +16,29 @@ print(f"{args.infected_person_login} tested positive on {args.day_positive}.")
 print(f"Gathering data from the following hosts: {args.hosts}")
 
 # Create an instance of DatabaseOperations to query some data from the database
-db_operations = OperationsDatabase("codam_corona_tracker", "data", "hilmi", "hilmi")
+db_operations = OperationsDatabase("codam_corona_tracker", "f0r1s8", "hilmi", "hilmi")
 
 # Create the query
-query_login = "select * from test_host where login = \'{}\'".format(
+query_login = "select * from f0r1s8 where login = \'{}\'".format(
     args.infected_person_login)
 print(query_login)
 
 # Store all the data of the infected person
-data = db_operations.read_data(query_login)
+data = db_operations.read(query_login)
 print(data)
 
 # Find for each login session of the infected person, which hosts where nearby
+for session in data: # loops over the sessions of the infected person
+	print(session)
+	begin_at = session[2]
+	end_at = session[3]
+	print(type(begin_at))
+	for contact in args.hosts: # loops over all hosts close to this specific session of the infected person
+		contact_query = """
+		select * from f0r1s8 where (host = \'{}\') and (begin_at or end_at between %s and %s)
+		""".format(contact)
+		contact_data = [begin_at, end_at]
 
+	print(session)
 
 # Make query to all those hosts within timeframe of log in period of infected person
